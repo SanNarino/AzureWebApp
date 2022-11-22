1c2a65587e9b8e5240b539e864f039c4332c0621
sannarino <einar.rahka@gmail.com>
2022-11-22 16:57:08 +0100

    Added createuser class

---
+++ AzureWebApp/CreateUser.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace AzureWebApp;
+
+public class CreateUser{
+    //method to create user with username
+    public void CreateUser(string username){
+        //code to create user
+    
+    }
+
+    public void EditUser(string username){  
+        //code to edit user
+    
+    }
+
+    //method to delete user from snowflake database
+    public void DeleteUser(string username){
+        //code to delete user
+    }
+
+    
+
+}
