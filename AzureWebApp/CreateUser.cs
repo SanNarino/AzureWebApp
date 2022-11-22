17d273977d089600333f57f3175b1ce521406feb
sannarino <einar.rahka@gmail.com>
2022-11-22 17:44:56 +0100

    HttpExample to Homepage

--- AzureWebApp/CreateUser.cs
+++ AzureWebApp/CreateUser.cs
@@ -3,20 +3,20 @@ using System;
 namespace AzureWebApp;
 
 public class CreateUser{
-    //method to create user with username
+
     public void createUser(string username){
-        //code to create user
+        
     
     }
 
     public void editUser(string username){  
-        //code to edit user
+        
     
     }
 
-    //method to delete user from snowflake database
+    
     public void deleteUser(string username){
-        //code to delete user
+        
     }
 
     
